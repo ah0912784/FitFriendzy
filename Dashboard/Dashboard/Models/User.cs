5f32868b0f276d83414601ee560c751db528c547
adkinsjack <jackadkins18@gmail.com>
2023-10-23 14:44:53 -0500

    user data model

--- Dashboard/Dashboard/Models/User.cs
+++ Dashboard/Dashboard/Models/User.cs
@@ -1,13 +1,28 @@
-﻿namespace Dashboard.Models
-{
-    public class User
-    {
-        private int Id { get; set; }
-        public string Name { get; set; }
+﻿namespace Dashboard.Models;
 
-        public int GetUserId()
-        {
-            return this.Id;
-        }
-    }
+public partial class User
+{
+    public Guid UserId { get; set; }
+
+    public string? UserDisplayName { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public int PhoneNumber { get; set; }
+
+    public virtual ICollection<Leaderboard> Leaderboards { get; set; } = new List<Leaderboard>();
+
+    public virtual ICollection<UserActivity> UserActivities { get; set; } = new List<UserActivity>();
+
+    public virtual ICollection<UserGroupMembership> UserGroupMemberships { get; set; } = new List<UserGroupMembership>();
+
+    public virtual ICollection<UserSetting> UserSettings { get; set; } = new List<UserSetting>();
 }
