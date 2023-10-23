9b094fede9de6fcc0d9bde3794c7b8383eb24c2d
adkinsjack <jackadkins18@gmail.com>
2023-10-23 14:45:16 -0500

    user setting data model

---
+++ Dashboard/Dashboard/Models/UserSetting.cs
@@ -0,0 +1,16 @@
+﻿namespace Dashboard.Models;
+
+public partial class UserSetting
+{
+    public Guid SettingsId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public bool Private { get; set; }
+
+    public string? NotificationPref { get; set; }
+
+    public string? ThemePref { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
