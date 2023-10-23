fe2e6b307b493352862a3f21b424e9b27015576d
adkinsjack <jackadkins18@gmail.com>
2023-10-23 14:44:35 -0500

    group data model

---
+++ Dashboard/Dashboard/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿namespace Dashboard.Models;
+
+public partial class Group
+{
+    public Guid GroupId { get; set; }
+
+    public Guid GroupLeaderId { get; set; }
+
+    public string GroupName { get; set; } = null!;
+
+    public virtual ICollection<UserGroupMembership> UserGroupMemberships { get; set; } = new List<UserGroupMembership>();
+}
