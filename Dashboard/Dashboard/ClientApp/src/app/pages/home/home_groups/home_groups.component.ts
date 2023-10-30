779ff28a1144800857a2028bb51d106dfed981eb
Dominic <dbell97@live.com>
2023-10-30 00:44:09 -0500

    Added homepage data interpolation

---
+++ Dashboard/Dashboard/ClientApp/src/app/pages/home/home_groups/home_groups.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-home_groups',
+  // styleUrls: ['./home_groups.component.scss'],
+  template: `
+      <nb-list>
+        <nb-list-item *ngFor="let group of groups">
+          {{ group }}
+          <button nbButton class="right" status='info'>View Group</button>
+        </nb-list-item>
+      </nb-list>
+  `,
+})
+
+export class HomeGroupComponent {
+  // Dummy data until we can properly pull it
+  groups = ["MSU Golf Friends", "Ragin' Tornados", "Wacky Warriors"]
+}
---
+++ Dashboard/Dashboard/ClientApp/src/app/pages/home/home_name/home_name.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-home_name',
+  // styleUrls: ['./home_activity.component.scss'],
+  template: `
+      {{firstName}} {{lastName}}
+  `,
+})
+
+export class HomeNameComponent {
+  // Dummy data until we can properly pull it
+  firstName = "John"
+  lastName = "Doe"
+}
