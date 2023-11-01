788e6292c1cbf6c0c4c0511ca44dfe3c79b6c2c6
ah0912784 <a.hansen354@gmail.com>
2023-10-31 23:56:40 -0500

    Moved profile-components folder into themes. Added index.ts to properly export components. Added imports to pages\profile\profile.components.ts file.

--- Dashboard/Dashboard/ClientApp/src/app/pages/profile/profile.component.ts
+++ Dashboard/Dashboard/ClientApp/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NbLayoutModule } from '@nebular/theme/components/layout/layout.module';
+import { ProfileBioComponent, ProfileGroupsComponent, ProfilePicComponent, ProfileStatsComponent } from '../../@theme/profile-components';
 
 @Component({
   selector: 'ngx-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
+
+  @NgModule({
+    imports: [
+      NbLayoutModule,
+      ProfileBioComponent,
+      ProfileGroupsComponent,
+      ProfilePicComponent,
+      ProfileStatsComponent,
+
+    ],
+  })
 export class ProfileComponent {
 
 }
