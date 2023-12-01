7b24c820436e8a9956874d81e03e694f006ee2af
Dominic <dbell97@live.com>
2023-12-01 17:32:55 -0600

    Changed dates to unix timestamps.

--- Dashboard/Dashboard/ClientApp/src/app/pages/home/home_progress/home_progress.component.ts
+++ Dashboard/Dashboard/ClientApp/src/app/pages/home/home_progress/home_progress.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 import { NbCalendarRange, NbDateService } from '@nebular/theme';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'ngx-home-progress',
@@ -11,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
       <nb-card-header>Goal Progress</nb-card-header>
       <nb-card-body>
         <p>
-          Your progress for your goal ending {{endDate}} is {{points_this_period}} out of {{goal}} points!
+          Your progress for your goal ending {{endDate | date:'longDate'}} is {{points_this_period}} out of {{goal}} points!
         </p>
         <nb-progress-bar [value]=point_goal_ratio [displayValue]="true" status="success"></nb-progress-bar>
     </nb-card-body>
@@ -25,7 +25,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
       Selected range: {{ range.start | date }} - {{ range.end | date }}
     </nb-card-header>
     <nb-card-body>
-      <nb-calendar-range [(range)]="range"></nb-calendar-range>
+      <nb-calendar-range [(range)]="range" format="dd.MM.yyyy"></nb-calendar-range>
     </nb-card-body>
     <nb-card-body>
     <nb-card>
@@ -55,7 +55,7 @@ export class HomeProgressComponent implements OnDestroy {
   goalPoints: number;
 
   range: NbCalendarRange<Date>;
-  constructor(private formBuilder: FormBuilder, protected dateService: NbDateService<Date>) {
+  constructor(private datePipe: DatePipe, private formBuilder: FormBuilder, protected dateService: NbDateService<Date>) {
     this.range = {
       start: this.dateService.addDay(this.monthStart, 3),
       end: this.dateService.addDay(this.monthEnd, -3),
@@ -76,10 +76,13 @@ export class HomeProgressComponent implements OnDestroy {
   onSubmit() {
     // retreive the start/end date and points
     const enteredPoints = this.goalForm.get('gpoints').value;
-    const startDate = this.range.start;
-    const endDate = this.range.end;
+    // To convert the dates to timestamps https://stackoverflow.com/questions/29935730/how-to-convert-yyyy-mm-dd-hhmmss-to-unix-timestamp-in-angularjs-or-javascript
+    const startDate = this.datePipe.transform(this.range.start, 'shortDate');
+    var unixStartDate = (new Date(startDate.replace('-', '/'))).getTime() / 1000;
+    const endDate = this.datePipe.transform(this.range.end, 'shortDate');
+    var unixEndDate = (new Date(endDate.replace('-', '/'))).getTime() / 1000;
     // log the variables for testing
-    console.log(`Goal Points: ${enteredPoints}, Start Date: ${startDate}, End Date: ${endDate}`);
+    console.log(`Goal Points: ${enteredPoints}, Unix Start Date: ${unixStartDate}, Unix End Date: ${unixEndDate}`);
     // TODO: use this information to push the new goal to the database instead of logging
 
   }
@@ -90,7 +93,13 @@ export class HomeProgressComponent implements OnDestroy {
   // db pull: select sum(points) from whereverPointsAreAt where userID="howeverthisworks" AND date between (startDate and endDate)
   // from these pulls, set the variables points_this_period and goal
   // Dummy data until we can properly pull it
-  endDate = 'Fri Dec 15 2023'
+
+  // Okay we can pull the unix date, for example
+  unixEndDate = '1701669600'
+  // then it will need to be * 1000
+  endDate = parseInt(this.unixEndDate) * 1000
+  // this ex results in December 4, 2023
+
   points_this_period = 30;
   goal = 100;
 
