40a8d2b8b8ec54cff871dbd214773f3e5a7c3f7e
Dominic <dbell97@live.com>
2023-12-01 16:57:06 -0600

    Initial goal setup completed. Needs DB connection

--- Dashboard/Dashboard/ClientApp/src/app/pages/home/home_progress/home_progress.component.ts
+++ Dashboard/Dashboard/ClientApp/src/app/pages/home/home_progress/home_progress.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
+import { NbCalendarRange, NbDateService } from '@nebular/theme';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'ngx-home-progress',
-  // styleUrls: ['./home_activity.component.scss'],
   template: `
-    <nb-card-body>
+  <nb-card size="small">
+      <nb-card-header>Goal Progress</nb-card-header>
+      <nb-card-body>
         <p>
-          Your progress for {{month}} is {{points_this_period}} out of {{goal}} points!
+          Your progress for your goal ending {{endDate}} is {{points_this_period}} out of {{goal}} points!
         </p>
-
-        <nb-progress-bar [value]="50" status="success"></nb-progress-bar>
+        <nb-progress-bar [value]=point_goal_ratio [displayValue]="true" status="success"></nb-progress-bar>
     </nb-card-body>
+  </nb-card>
+  
+  <nb-card>
+    <nb-card-header>
+      Select New Goal
+    </nb-card-header>
+    <nb-card-header>
+      Selected range: {{ range.start | date }} - {{ range.end | date }}
+    </nb-card-header>
+    <nb-card-body>
+      <nb-calendar-range [(range)]="range"></nb-calendar-range>
+    </nb-card-body>
+    <nb-card-body>
+    <nb-card>
+    <form [formGroup]="goalForm" (ngSubmit)="onSubmit()">
+        <div class="form-group">
+          <label for="gpoints">Goal Points: </label>
+          <input type="number" placeholder="Points To Earn" formControlName="gpoints" nbInput>
+        </div>
+      <button nbButton status="primary" type="submit">Set Goal</button>
+    </form>
+  </nb-card>  
+  
+    
   `,
   styles: [`
     * {
@@ -24,13 +51,51 @@ import { Subject } from 'rxjs';
 export class HomeProgressComponent implements OnDestroy {
   protected readonly destroying$ = new Subject<void>();
 
+  goalForm: FormGroup;
+  goalPoints: number;
 
+  range: NbCalendarRange<Date>;
+  constructor(private formBuilder: FormBuilder, protected dateService: NbDateService<Date>) {
+    this.range = {
+      start: this.dateService.addDay(this.monthStart, 3),
+      end: this.dateService.addDay(this.monthEnd, -3),
+    };
+    this.goalForm = this.formBuilder.group({
+      gpoints: [null, Validators.required]
+    })
+  }
+
+  get monthStart(): Date {
+    return this.dateService.getMonthStart(new Date());
+  }
+
+  get monthEnd(): Date {
+    return this.dateService.getMonthEnd(new Date());
+  }
+
+  onSubmit() {
+    // retreive the start/end date and points
+    const enteredPoints = this.goalForm.get('gpoints').value;
+    const startDate = this.range.start;
+    const endDate = this.range.end;
+    // log the variables for testing
+    console.log(`Goal Points: ${enteredPoints}, Start Date: ${startDate}, End Date: ${endDate}`);
+    // TODO: use this information to push the new goal to the database instead of logging
+
+  }
+
+  // TODO:
+  // Now, we need to PULL this data each time to update the goalbar component
+  // db pull: select startDate, endDate, goalPoints from users where userID="howeverthisworks"
+  // db pull: select sum(points) from whereverPointsAreAt where userID="howeverthisworks" AND date between (startDate and endDate)
+  // from these pulls, set the variables points_this_period and goal
   // Dummy data until we can properly pull it
-  month = "October";
-  points_this_period = 50;
+  endDate = 'Fri Dec 15 2023'
+  points_this_period = 30;
   goal = 100;
-  // Cant seem to pass this for value in nb-progress-bar
-  point_goal_ratio = ((this.points_this_period / this.goal) * 100).toString;
+
+  // Used to create the progress bar
+  point_goal_ratio = ((this.points_this_period / this.goal) * 100);
 
   ngOnDestroy(): void {
     this.destroying$.next();
