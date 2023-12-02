c63f3073e9ea9e1817a342162e2c38a4cff9c7f0
adkinsjack <jackadkins18@gmail.com>
2023-12-01 23:15:29 -0600

    user goals: goals model

---
+++ Dashboard/Dashboard/Models/UserGoal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dashboard.Models;
+
+public partial class UserGoal
+{
+    public Guid GoalId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public long StartTime { get; set; }
+
+    public long EndTime { get; set; }
+
+    public int TargetPoints { get; set; }
+
+    public int CurrentPoints { get; set; }
+}
